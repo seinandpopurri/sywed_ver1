3a958b8d0b3a836c609e2a0947224420438e7e5d
seinandpopurri <meatandvegetarian@gmail.com>
2025-04-20 10:04:15 +0900

    가족관계도 계좌번호 수정

--- src/pages/FamilyTree.tsx
+++ src/pages/FamilyTree.tsx
@@ -20,8 +20,8 @@ const FamilyTree: React.FC<FamilyTreeProps> = ({ onBack }) => {
 
   // 가족 구성원 데이터
   const familyMembers: FamilyMember[] = [
-    { id: "yangkyunghee", name: "양경희", relation: "이은지, 이영건의 엄마", phone: "010-2017-7716", bankAccount: "000-0000-0000 신한" },
-    { id: "leejungkil", name: "이정길", relation: "이은지, 이영건의 아빠", phone: "010-2354-9962", bankAccount: "000-0000-0000 국민" },
+    { id: "yangkyunghee", name: "양경희", relation: "이은지, 이영건의 엄마", phone: "010-2017-7716", bankAccount: "601154-56-184536 농협 (예금주: 이은지)" },
+    { id: "leejungkil", name: "이정길", relation: "이은지, 이영건의 아빠", phone: "010-2354-9962", bankAccount: "352-1320-2950-33 농협 (예금주: 이은지)" },
     { id: "leeunji", name: "이은지", relation: "양경희, 이정길의 딸\n이영건의 누나\n손상옥의 아내\n손현빈의 엄마", isChild: true },
     { id: "leeyounggun", name: "이영건", relation: "양경희, 이정길의 아들\n이은지의 동생", phone: "010-5104-9962", bankAccount: "110-274-870261 신한" },
     { id: "kimsieun", name: "김시은", relation: "홍세인, 홍세영의 엄마", phone: "010-9209-4919", bankAccount: "352-1887-6759-73 농협" },
